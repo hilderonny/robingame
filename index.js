7cbf78ffba75ec32394873cfe1cb0dc4dbccbf9c
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-04-30 19:49:51 +0200

    HTTPS_PORT als Umgebungsvariable aktiviert.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var credentials = {
     cert: fs.existsSync('./pub.cert') ? fs.readFileSync('./pub.cert', 'utf8') : null
 };
 
+var httpsPort = process.env.HTTPS_PORT || 443;
+
 var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(443, function() {
-    console.log(`HTTPS laeuft an Port 443.`);
+httpsServer.listen(httpsPort, function() {
+    console.log(`HTTPS laeuft an Port ${httpsPort}.`);
 });
