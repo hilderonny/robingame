f87f67e77e8cb45e1fb6e9741d3fe255e409f749
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-05-08 11:45:14 +0200

    EIn Würfel kann bewegt werden.

--- public/babylon/index.js
+++ public/babylon/index.js
@@ -1,28 +1,65 @@
+var scene, vrHelper, meshestointeract = [], dragobject;
 
-/******* Add the create scene function ******/
+function addinteractions(interactionmanager, targetmesh) {
+    console.log(interactionmanager, targetmesh);
+    interactionmanager.registerAction(new BABYLON.ExecuteCodeAction({
+        trigger: BABYLON.ActionManager.OnIntersectionEnterTrigger, parameter: targetmesh
+    }, function(evt) {
+        targetmesh.material.wireframe = true;
+        dragobject = targetmesh;
+    }));
+    interactionmanager.registerAction(new BABYLON.ExecuteCodeAction({
+        trigger: BABYLON.ActionManager.OnIntersectionExitTrigger, parameter: targetmesh
+    }, function(evt) {
+        targetmesh.material.wireframe = false;
+        dragobject = null;
+    }));
+}
+
+function createhand(controller) {
+    var hand = BABYLON.MeshBuilder.CreateSphere("hand", { segments: 1, diameter: 0.1 }, scene);
+    hand.position = new BABYLON.Vector3(controller.hand === "right" ? 0.01 : -0.01, -0.02, -0.04);
+    hand.parent = controller.mesh;
+    controller.onMainButtonStateChangedObservable.add(function(stateObject) {
+        if (!dragobject) return;
+        dragobject.setParent(stateObject.pressed ? hand : null);
+    });
+    hand.actionManager = new BABYLON.ActionManager(scene);
+    meshestointeract.forEach(function (m) {
+        addinteractions(hand.actionManager, m);
+    });
+}
+
+// Panel which can be grabbed on the edges and moved
+function grabdemo() {
+
+    var boundingbox = BABYLON.MeshBuilder.CreateBox("boundingbox", { size: .2 }, scene);
+    boundingbox.position = new BABYLON.Vector3(0, 1.5, 0.3);
+    boundingbox.material = new BABYLON.StandardMaterial("boundingboxmaterial", scene);
+    boundingbox.material.emissiveColor = new BABYLON.Color3(0, 0, 1);
+
+    meshestointeract.push(boundingbox);
+}
+
 var createScene = function (canvas, engine) {
 
     // Create the scene space
-    var scene = new BABYLON.Scene(engine);
-    var vrHelper = scene.createDefaultVRExperience();
+    scene = new BABYLON.Scene(engine);
 
-    // Add and manipulate meshes in the scene
-    // var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: .5 }, scene);
-    // sphere.position.y = 1;
-    // sphere.position.z = 1;
+    vrHelper = scene.createDefaultVRExperience();
+    vrHelper.onControllerMeshLoaded.add(createhand);
+
+    grabdemo();
 
     // Light
-    var spot = new BABYLON.PointLight("spot", new BABYLON.Vector3(0, 30, 10), scene);
+    var spot = new BABYLON.PointLight("spot", new BABYLON.Vector3(-10, 30, -10), scene);
     spot.diffuse = new BABYLON.Color3(1, 1, 1);
     spot.specular = new BABYLON.Color3(0, 0, 0);
 
     // Camera
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 100, BABYLON.Vector3.Zero(), scene);
-    camera.lowerBetaLimit = 0.1;
-    camera.upperBetaLimit = (Math.PI / 2) * 0.9;
-    camera.lowerRadiusLimit = 30;
-    camera.upperRadiusLimit = 150;
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 100, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
+    // scene.activeCamera.minZ = 0;
 
     // Ground
     var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
@@ -31,13 +68,8 @@ var createScene = function (canvas, engine) {
     var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "textures/worldHeightMap.jpg", 20, 20, 250, 0, 1, scene, false);
     ground.material = groundMaterial;
 
-    vrHelper.enableTeleportation({floorMeshName: "ground"});
-    vrHelper.teleportationEnabled = true;
-
-    //Sphere to see the light's position
-    // var sun = BABYLON.Mesh.CreateSphere("sun", 10, 4, scene);
-    // sun.material = new BABYLON.StandardMaterial("sun", scene);
-    // sun.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
+    // vrHelper.enableTeleportation({floorMeshName: "ground"});
+    // vrHelper.teleportationEnabled = true;
 
     // Skybox
     var skybox = BABYLON.Mesh.CreateBox("skyBox", 800.0, scene);
@@ -50,14 +82,6 @@ var createScene = function (canvas, engine) {
     skyboxMaterial.disableLighting = true;
     skybox.material = skyboxMaterial;
 
-    //Sun animation
-    // scene.registerBeforeRender(function () {
-    //     sun.position = spot.position;
-    //     spot.position.x -= 0.5;
-    //     if (spot.position.x < -90)
-    //         spot.position.x = 100;
-    // });
-
     return scene;
 };
 
@@ -66,7 +90,7 @@ window.addEventListener("load", function () { // Watch for browser/canvas resize
     var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
     var scene = createScene(canvas, engine); //Call the createScene function
-    
+
     window.addEventListener("resize", function () { // Watch for browser/canvas resize events
         engine.resize();
     });
