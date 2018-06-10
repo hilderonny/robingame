2e614dad1036776484f4a1498789b5e41e8435e5
hilderonny <ronny.hildebrandt@avorium.de>
2018-06-10 22:12:56 +0200

    Added player radius and spot.

--- public/siedler/js/cesium.js
+++ public/siedler/js/cesium.js
@@ -19,7 +19,7 @@ class App {
             navigationInstructionsInitiallyVisible: false,
             scene3DOnly: true,
             imageryProvider: Cesium.createOpenStreetMapImageryProvider({
-                url : 'https://a.tile.openstreetmap.org/'
+                url: 'https://a.tile.openstreetmap.org/'
                 // url: 'https://stamen-tiles.a.ssl.fastly.net/watercolor/',
                 // maximumLevel: 16,
             }),
@@ -28,8 +28,30 @@ class App {
         this.viewer.scene.fog.density = 0.02;
         this.viewer.camera.flyHome = this.goToPlayerPosition;
         this.goToPlayerPosition();
+
+        this.playerRadius = this.viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(0, 0),
+            ellipse: {
+                semiMinorAxis: 50.0,
+                semiMajorAxis: 50.0,
+                material: Cesium.Color.BLUE.withAlpha(0.3)
+            }
+        });
+
+        this.playerSpot = this.viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(0, 0),
+            ellipse: {
+                semiMinorAxis: 2.0,
+                semiMajorAxis: 2.0,
+                material: new Cesium.ImageMaterialProperty({
+                    image: "geolocation_marker.png",
+                    transparent: true,
+                })
+            }
+        });
+
     }
-    
+
     flyToGeoLocation(geolocation) {
         var self = this;
         var pointOfInterest = Cesium.Cartographic.fromDegrees(
@@ -39,28 +61,42 @@ class App {
             new Cesium.Cartographic());
         // Cesium.sampleTerrain(self.viewer.terrainProvider, 9, [pointOfInterest]).then(function (samples) {
         //     var height = samples && samples[0] && samples[0].height ? samples[0].height : 0.0;
-            self.viewer.camera.flyTo({
-                destination: Cesium.Cartesian3.fromDegrees(
-                    geolocation.coords.longitude,
-                    geolocation.coords.latitude,
-                    300
-                    // height + 500
-                )
-            });
+        self.viewer.camera.flyTo({
+            destination: Cesium.Cartesian3.fromDegrees(
+                geolocation.coords.longitude,
+                geolocation.coords.latitude,
+                300
+                // height + 500
+            )
+        });
         // });
     }
 
     goToPlayerPosition() {
         var self = this;
-        navigator.geolocation.getCurrentPosition(function(geolocation) {
+        navigator.geolocation.getCurrentPosition(function (geolocation) {
             self.flyToGeoLocation(geolocation);
         });
+    }
+
+    updatePlayerPosition(geolocation) {
+        var position = Cesium.Cartesian3.fromDegrees(
+            geolocation.coords.longitude,
+            geolocation.coords.latitude,
+            0
+        );
+        this.playerRadius.position = position;
+        this.playerSpot.position = position;
     }
 
 }
 
 var app;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     app = new App("cesiumContainer");
+
+    navigator.geolocation.watchPosition(function(geolocation) {
+        app.updatePlayerPosition(geolocation);
+    });
 });
